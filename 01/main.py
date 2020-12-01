c422aabb9c26fdf1d46c0fa9daab2f4e47aa9ea4
Victor Xing <victor.t.xing@gmail.com>
2020-12-01 14:56:07 +0100

    Complete day 1

---
+++ 01/main.py
@@ -0,0 +1,17 @@
+with open('input.txt') as f:
+    lines = [int(line.rstrip()) for line in f]
+
+print(lines)
+
+for i in range(len(lines)):
+    for j in range(i+1, len(lines)):
+        if (lines[i]+lines[j] == 2020):
+            print(lines[i]*lines[j])
+            break
+
+for i in range(len(lines)):
+    for j in range(i+1, len(lines)):
+        for k in range(j+1, len(lines)):
+            if (lines[i]+lines[j]+lines[k] == 2020):
+                print(lines[i]*lines[j]*lines[k])
+                break
